f735adef70f61a870ddaa3ffe2d822b842af0c92
Snakevil Zen <zsnakevil@gmail.com>
2012-09-29 18:41:59 +0800

    Added parsers for `zongheng.com'.
    Signed-off-by: Snakevil Zen <zsnakevil@gmail.com>

---
+++ lib/model/zongheng.com/chapter.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Represents as a novel chapter in `zongheng.com'.
+ *
+ * This file is part of NOVEL.READER.
+ *
+ * NOVEL.READER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NOVEL.READER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NOVEL.READER.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   novel.reader
+ * @author    Snakevil Zen <zsnakevil@gmail.com>
+ * @copyright © 2012 snakevil.in
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+namespace NrModel\Zongheng_com;
+
+use Exception;
+use NrModel;
+
+class Chapter extends NrModel\Chapter
+{
+    /**
+     * Defines the matched URL pattern.
+     *
+     * INHERITED from {@link NrModel\Page::PATTERN}.
+     *
+     * @var string
+     */
+    const PATTERN = '~^http://book\.zongheng\.com/chapter/\d+/\d+\.html$~';
+
+    /**
+     * Parses retrieved content into meta-data.
+     *
+     * OVERRIDEN FROM {@link NrModel\Page::parse()}.
+     *
+     * @param  string  $content
+     * @return Chapter
+     */
+    protected function parse($content)
+    {
+        settype($content, 'string');
+        $s_ret = $this->crop('@<body bookId="\d+" bookName="@', '@"@', $content);
+        if (false === $s_ret)
+            return $this;
+        $this->novelTitle = $s_ret;
+        $s_ret = $this->crop('@<h2>.*<em>@', '@</em></h2>@', $content);
+        if (false === $s_ret)
+            return $this;
+        $this->title = $s_ret;
+        $s_ret = $this->crop('@<div id="chapterContent" class="content"><p>@', '@</p></div>@', $content);
+        if (false === $s_ret)
+            return $this;
+        $this->paragraphs = array();
+        $a_tmp = preg_split('@(<span class="watermark">.*</span>)?</p><p>@U', $s_ret);
+        for ($ii = 0, $jj = count($a_tmp); $ii < $jj; $ii++)
+        {
+            $a_tmp[$ii] = trim($a_tmp[$ii]);
+            if (strlen($a_tmp[$ii]))
+                $this->paragraphs[] = $a_tmp[$ii];
+        }
+        array_push($this->paragraphs, array_shift(explode('<span ', array_pop($this->paragraphs))));
+        $s_ret = $this->crop('@\(快捷键：←\)<a href="http://book.zongheng.com/chapter/\d+/@', '@">上一章</a>@', $content);
+        $this->prevLink = false === $s_ret ? '' : $s_ret;
+        $s_ret = $this->crop('@\(快捷键：回车\)<a href="http://book.zongheng.com@', '@">回目录</a>@', $content);
+        if (false === $s_ret)
+            return $this;
+        $this->tocLink = '../..' . $s_ret;
+        $s_ret = $this->crop('@\(快捷键：→\)<a href="http://book.zongheng.com/chapter/\d+/@', '@">下一章</a>@', $content);
+        $this->nextLink = false === $s_ret ? '' : $s_ret;
+        return $this;
+    }
+}
+
+# vim:se ft=php ff=unix fenc=utf-8 tw=120:
---
+++ lib/model/zongheng.com/toc.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Represents as a novel TOC page in `zongheng.com'.
+ *
+ * This file is part of NOVEL.READER.
+ *
+ * NOVEL.READER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NOVEL.READER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NOVEL.READER.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   novel.reader
+ * @author    Snakevil Zen <zsnakevil@gmail.com>
+ * @copyright © 2012 snakevil.in
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+namespace NrModel\Zongheng_com;
+
+use Exception;
+use NrModel;
+
+class TOC extends NrModel\TOC
+{
+    /**
+     * Defines the matched URL pattern.
+     *
+     * INHERITED from {@link NrModel\Page::PATTERN}.
+     *
+     * @var string
+     */
+    const PATTERN = '~^http://book\.zongheng\.com/showchapter/\d+.html$~';
+
+    /**
+     * Parses retrieved content into meta-data.
+     *
+     * OVERRIDEN FROM {@link NrModel\Page::parse()}.
+     *
+     * @param  string  $content
+     * @return TOC
+     */
+    protected function parse($content)
+    {
+        settype($content, 'string');
+        $s_ret = $this->crop('@<meta name="keywords" content="@', '@,最新章节,最新TXT下载,全文阅读@', $content);
+        if (false === $s_ret)
+            return $this;
+        list($this->title, $this->author) = explode(',', $s_ret);
+        $s_ret = $this->crop('@<div class="chapter">@', '@<!-- 章节列表 结束 -->@', $content);
+        if (false === $s_ret ||
+            false === preg_match_all('@<td><a href="http://book.zongheng.com(.*)" title="最后更新时间:.*">(.*)</a></td>@U', $s_ret, $a_tmp)
+        )
+            return $this;
+        $this->chapters = array();
+        for ($ii = 0, $jj = count($a_tmp[1]); $ii < $jj; $ii++)
+        {
+            $this->chapters['..' . $a_tmp[1][$ii]] = $a_tmp[2][$ii];
+        }
+        return $this;
+    }
+}
+
+# vim:se ft=php ff=unix fenc=utf-8 tw=120:
