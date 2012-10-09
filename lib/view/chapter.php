82149c9ff09f85083db6d2cfee07eb28c2f64d36
Snakevil Zen <zsnakevil@gmail.com>
2012-10-09 18:03:05 +0800

    Rewriten the prefetch feature.
    Signed-off-by: Snakevil Zen <zsnakevil@gmail.com>

--- lib/view/chapter.php
+++ lib/view/chapter.php
@@ -55,7 +55,6 @@ class Chapter extends Page
         $s_paragraphs = implode("</p>\n<p>", $this->page->paragraphs);
         if (false !== strpos($s_paragraphs, '![IMAGE]('))
             $s_paragraphs = preg_replace('@!\[IMAGE\]\((\S+)\)@U', '<img src="$1" />', $s_paragraphs);
-        $a_tmp = count_chars($this->page->url, 1);
         return <<<HTML
 <!DOCTYPE html>
 <html>
@@ -67,36 +66,11 @@ class Chapter extends Page
 <link rel="icon" href="{$this->uri}share/ccnr.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="{$this->uri}share/ccnr.ico" type="image/x-icon" />
 <script>
-_ = {
-    v : false,
-    l : location.href,
-    x : "{$this->page->tocLink}",
-    s : {
-        t : [document.title, ""],
-        b : "",
-        p : "{$this->page->prevLink}",
-        n : "{$this->page->nextLink}",
-        r : "{$this->page->url}"
-    }
-};
-function navpage(ev) {
-    ev = ev || window.event;
-    switch (ev.keyCode) {
-        case 13:
-            location.href = _.x;
-            break;
-        case 37:
-            if (document.getElementById("prevLink"))
-                document.getElementById("prevLink").click();
-            else if (_.s.p.length)
-                location.href = _.s.p;
-            break;
-        case 39:
-            if (document.getElementById("nextLink"))
-                document.getElementById("nextLink").click();
-            else if (_.s.n.length)
-                location.href = _.s.n;
-            break;
+_={\$:function(x){return document.getElementById(x)},a:"{$this->uri}api/prefetch?s=",x:"{$this->page->tocLink}",p:"{$this->page->prevLink}",n:"{$this->page->nextLink}",r:"{$this->page->url}"};
+function navpage(ev){ev=ev||window.event;switch(ev.keyCode){
+    case 13:location.href=_.x;break;
+    case 37:if(_.$("prevLink"))_.$("prevLink").click();else if(_.p.length)location.href=_.p;break;
+    case 39:if(_.$("nextLink"))_.$("nextLink").click();else if(_.n.length)location.href=_.n;break;
 }}
 </script>
 </head>
@@ -111,102 +85,7 @@ function navpage(ev) {
 <li><a id="prevLink" href="{$this->page->prevLink}">上一章</a></li>
 <li class="nextLink"><a id="nextLink" href="{$this->page->nextLink}">下一章</a></li>
 </ul>
-<script>
-if (_.s.n.length) {
-    if (history.pushState) {
-        (function () {
-            _.s.t[1] = document.getElementById("tocLink").textContent,
-            _.s.b = document.getElementsByTagName("blockquote")[0].innerHTML,
-            _.c = function (x, y) {
-                var i = x.split("/"),
-                    j = y.split("/");
-                i.pop();
-                for (var k = 0; k < j.length; k++)
-                    if (".." == j[k] && 3 < i.length)
-                        i.pop();
-                    else
-                        i.push(j[k]);
-                return i.join("/");
-            },
-            _.d = function (x) {
-                with (document) {
-                    with (getElementsByTagName("blockquote")[0]) {
-                        innerHTML = x.b;
-                        setAttribute("cite", x.r);
-                    }
-                    title = x.t[0],
-                    getElementById("tocLink").textContent = x.t[1],
-                    getElementById("prevLink").href = x.p,
-                    getElementById("nextLink").href = x.n;
-                }
-            },
-            _.g = function (x, y) {
-                var i = x, x = _.c(y, x), y = i, i = new XMLHttpRequest;
-                i.onload = function () {
-                    try {
-                        var j = JSON.parse(i.responseText);
-                        if (200 != j.code || x != j.referer) {
-                            document.getElementById("nextLink").href = "#" + y;
-                            return;
-                        }
-                        _.t = {
-                            t : [
-                                j.data.novelTitle + " - " + j.data.title + " * CCNR",
-                                j.data.title
-                            ],
-                            b : "",
-                            p : j.data.links.previous,
-                            n : j.data.links.next,
-                            r : x,
-                            i : y
-                        };
-                        var k = [];
-                        for (var l = 0; l < j.data.paragraphs.length; l++)
-                            if ("![IMAGE](" == j.data.paragraphs[l].substr(0, 9)) {
-                                k[k.length] = j.data.paragraphs[l].substring(9, j.data.paragraphs[l].length - 1);
-                                _.t.b += "<p><img src=\"" + k[k.length - 1] + "\"/></p>";
-                            } else
-                                _.t.b += "<p>" + j.data.paragraphs[l] + "</p>";
-                        document.body.appendChild(document.createElement("fieldset")).
-                            appendChild(document.createElement("img")).
-                            src = k[0];
-                    } catch (ex) {
-                    }
-                };
-                i.open("GET", "{$this->uri}api/prefetch?s="+x, true);
-                i.send();
-            },
-            document.getElementById("nextLink").onclick = function (ev) {
-                if (!_.t)
-                    return;
-                ev.preventDefault();
-                if (_.d.l)
-                    return;
-                _.d.l = _.t.i;
-                var t = _.t;
-                history.pushState(t, t.t[0], t.i);
-                _.d(t);
-                delete _.d.l;
-                delete _.t;
-                _.g(t.n, t.r);
-            },
-            window.onpopstate = function (ev) {
-                if (!_.v) {
-                    _.v = true;
-                    return;
-                }
-                _.d(null == ev.state ? _.s : ev.state);
-            };
-            if (_.s.n.length && '#' != _.s.n[0])
-                _.g(_.s.n, _.s.r);
-        })();
-    } else {
-        (function (x) {
-            x.body.appendChild(x.createElement("fieldset")).appendChild(x.createElement("img")).src = _.n;
-        })(document);
-    }
-}
-</script>
+<script type="text/javascript" src="{$this->uri}share/prefetch.js"></script>
 <noscript><fieldset><img src="{$this->page->nextLink}" /></fieldset></noscript>
 </body>
 </html>
---
+++ share/prefetch.js
@@ -0,0 +1,137 @@
+/**
+ * Prefetches the content of next chapter page.
+ *
+ * This file is part of NOVEL.READER.
+ *
+ * NOVEL.READER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NOVEL.READER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NOVEL.READER.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   novel.reader
+ * @author    Snakevil Zen <zsnakevil@gmail.com>
+ * @copyright © 2012 snakevil.in
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+if (_ && _.$ && _.n && _.n.length && "#" != _.n[0]) {
+	_.l = function (x) {
+		var y = document;
+		if ("FIELDSET" == y.body.lastChild.nodeName)
+			y.body.removeChild(y.body.lastChild);
+		y.body.appendChild(y.createElement("fieldset")).appendChild(y.createElement("img")).src = x;
+	}
+	if (history.pushState)
+		(function () {
+			_.c = function (x, y) {
+				var i = x.split("/"), j = y.split("/");
+				i.pop();
+				for (var k = 0; k < j.length; k++)
+					if (".." == j[k] && 3 < i.length)
+						i.pop();
+					else
+						i.push(j[k]);
+				return i.join("/");
+			};
+			_.d = function (x) {
+				with (document.getElementsByTagName("blockquote")[0]) {
+					innerHTML = x.b;
+					setAttribute("cite", x.r);
+				}
+				_.r = x.r;
+				document.title = x.t[0];
+				_.$("tocLink").textContent = x.t[1];
+				_.$("prevLink").href = _.p = x.p;
+				_.$("nextLink").href = _.n = x.n;
+			};
+			_.g = function (x, y) {
+				var i = _.c(location.href, x);
+				if (_.h.has(i)) return;
+				var j = new XMLHttpRequest;
+				j.onload = function () {
+					try {
+						var k = JSON.parse(j.responseText);
+						if (200 != k.code || _.c(y, x) != k.referer) {
+							_.$("nextLink").href = "#" + x;
+							return;
+						}
+						for (var l = [], m = "", n = 0; n < k.data.paragraphs.length; n++)
+							if ("![IMAGE](" == k.data.paragraphs[n].substr(0, 9)) {
+								l[l.length] = k.data.paragraphs[n].substring(9, k.data.paragraphs[n].length - 1);
+								m += "<p><img src=\"" + l[l.length - 1] + "\"/></p>";
+							} else
+								m += "<p>" + k.data.paragraphs[n] + "</p>";
+						_.h.push(i, {
+							b : m,
+							n : k.data.links.next,
+							r : k.referer,
+							t : [
+								k.data.novelTitle + " - " + k.data.title + " * CCNR",
+								k.data.title
+							],
+							p : k.data.links.previous
+						});
+						if (l.length)
+							_.l(l[0]);
+					} catch (ex) {}
+				};
+				j.open("GET", _.a + _.c(y, x), true);
+				j.send();
+			};
+			_.h = new (function () {
+				this.index = [location.href];
+				this.data = {};
+				this.data[location.href] = {
+					b : document.getElementsByTagName("blockquote")[0].innerHTML,
+					n : _.n,
+					r : _.r,
+					t : [document.title, _.$("tocLink").textContent],
+					p : _.p
+				};
+				this.get = function (x) {
+					if (this.has(x)) return this.data[x];
+				};
+				this.has = function (x) {
+					return !!this.data[x];
+				};
+				this.push = function (x, y) {
+					this.index[this.index.length] = x;
+					this.data[x] = y;
+					return this;
+				}
+			});
+			_.$("nextLink").onclick = function (ev) {
+				var x = this.href;
+				if (!_.h.has(x)) return;
+				var y = _.h.get(x);
+				ev.preventDefault();
+				if (this.onclick.locked) return;
+				this.onclick.locked = true;
+				history.pushState(y, y.t[0], x);
+				_.d(y);
+				delete this.onclick.locked;
+				_.g(y.n, _.r);
+			};
+			window.onpopstate = function (ev) {
+				if (!this.onpopstate.loaded) {
+					this.onpopstate.loaded = true;
+					return;
+				}
+				_.d(!ev.state ? _.h.get(location.href) : ev.state);
+			};
+			history.replaceState(_.h.get(location.href), document.title, location.href);
+			_.g(_.n, _.r);
+		})();
+	else
+		_.l(_.n);
+}
+
+// vim: se ft=javascript ff=unix fenc=utf-8 tw=120 sw=2 ts=2 sts=2 noet:
