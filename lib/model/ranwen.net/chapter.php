ecc67739016c001c35a8f549be655d9abd2c3d34
Snakevil Zen <zsnakevil@gmail.com>
2012-10-19 18:03:16 +0800

    Added parsers for `ranwen.net'.
    Signed-off-by: Snakevil Zen <zsnakevil@gmail.com>

---
+++ lib/model/ranwen.net/chapter.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Represents as a novel chapter in `ranwen.net'.
+ *
+ * This file is part of NOVEL.READER.
+ *
+ * NOVEL.READER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NOVEL.READER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NOVEL.READER.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   novel.reader
+ * @author    Snakevil Zen <zsnakevil@gmail.com>
+ * @copyright © 2012 szen.in
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+namespace CCNR\Model\Ranwen_net;
+
+use CCNR\Model;
+
+class Chapter extends Model\Chapter
+{
+    /**
+     * Defines the matched URL pattern.
+     *
+     * INHERITED from {@link Model\Page::PATTERN}.
+     *
+     * @var string
+     */
+    const PATTERN = '~^http://www\.ranwen\.net/files/article/\d+/\d+/\d+\.html$~';
+
+    /**
+     * Parses retrieved content into meta-data.
+     *
+     * OVERRIDEN FROM {@link Model\Page::parse()}.
+     *
+     * @param  string  $content
+     * @return Chapter
+     */
+    protected function parse($content)
+    {
+        settype($content, 'string');
+        $content = iconv('gbk', 'utf-8//ignore', $content);
+        $s_ret = $this->crop('@var articlename=\'@', '@\';@', $content);
+        if (false === $s_ret)
+            throw new Model\NovelTitleNotFoundException;
+        $this->novelTitle = $s_ret;
+        $s_ret = $this->crop('@var chaptername=\'@', '@\';@', $content);
+        if (false === $s_ret)
+            throw new Model\ChapterTitleNotFoundException;
+        $this->title = $s_ret;
+        $this->tocLink = 'index.html';
+        $s_ret = $this->crop('@var preview_page = "@', '@";@', $content);
+        if (false === $s_ret)
+            throw new Model\PrevLinkNotFoundException;
+        $this->prevLink = $s_ret;
+        if ('index.html' == $this->prevLink)
+            $this->prevLink = '';
+        $s_ret = $this->crop('@var next_page = "@', '@";@', $content);
+        if (false === $s_ret)
+            throw new Model\NextLinkNotFoundException;
+        $this->nextLink = $s_ret;
+        if ('index.html' == $this->nextLink)
+            $this->nextLink = '';
+        $s_ret = $this->crop('@<script src="/ssi/style-gg.js" type="text/javascript"></script></div>\s*(&nbsp;)*@', '@</DIV>@', $content);
+        if (false === $s_ret)
+            throw new Model\ParagraphsNotFoundException;
+        $this->paragraphs = array();
+        $a_tmp = preg_split('@(<br />)+(&nbsp;)*@', $s_ret);
+        for ($ii = 0, $jj = count($a_tmp); $ii < $jj; $ii++)
+        {
+            $a_tmp[$ii] = preg_replace('@^[　]+@u', '', $a_tmp[$ii]);
+            if (strlen($a_tmp[$ii]))
+                $this->paragraphs[] = $a_tmp[$ii];
+        }
+        if (empty($this->paragraphs))
+            throw new Model\ParagraphsNotFoundException;
+        return $this;
+    }
+}
+
+# vim:se ft=php ff=unix fenc=utf-8 tw=120:
