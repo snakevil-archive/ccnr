e603a79f4e53a9c10e965cc740594670dfb082cd
Snakevil Zen <zsnakevil@gmail.com>
2012-10-20 22:33:06 +0800

    Updated parsers of `shanwen.com' to support novels volumes.
    Signed-off-by: Snakevil Zen <zsnakevil@gmail.com>

--- lib/model/shanwen.com/toc.php
+++ lib/model/shanwen.com/toc.php
@@ -59,15 +59,44 @@ class TOC extends Model\TOC
             throw new Model\AuthorNotFoundException;
         $this->author = $s_ret;
         $s_ret = $this->crop('@<div class="bookdetail">@', '@<tr><td colspan="4" class="vcss">@', $content);
-        if (false === $s_ret ||
-            false === preg_match_all('@<td class="p88" width="250">\s*<a style="" href="(\d+\.html)">(.*)</a>\s*</td>@U', $s_ret, $a_tmp)
-        )
+        if (false === $s_ret)
             throw new Model\ChaptersListingNotFoundException;
+        $content = $s_ret . '<td height="20"';
         $this->chapters = array();
-        for ($ii = 0, $jj = count($a_tmp[1]); $ii < $jj; $ii++)
+        do
         {
-            $this->chapters[$a_tmp[1][$ii]] = $a_tmp[2][$ii];
+            $s_vol = $this->crop('@<strong>@', '@</strong>@', $content);
+            if (false === $s_vol)
+                break;
+            $s_ret = $this->crop('@</td>@', '@<td height="20"@', $content);
+            if (false === $s_ret ||
+                false === preg_match_all('@<a style="" href="(\d+\.html)">(.*)</a>@U', $s_ret, $a_tmp)
+            )
+                throw new Model\ChaptersListingNotFoundException(array('volume' => $s_vol));
+            $a_chps = array();
+            if (array_key_exists($s_vol, $this->chapters))
+            {
+                if (array_pop(array_keys($this->chapters)) == $s_vol)
+                    $a_chps = $this->chapters[$s_vol];
+                else
+                {
+                    while (array_key_exists($s_vol, $this->chapters))
+                    {
+                        list($s_vol, $ii) = explode('#', $s_vol);
+                        if (!$ii)
+                            $ii = 1;
+                        $s_vol .= '#' . (1 + $ii);
+                    }
+                }
+            }
+            $a_chps = array();
+            for ($ii = 0, $jj = count($a_tmp[1]); $ii < $jj; $ii++)
+                $a_chps[$a_tmp[1][$ii]] = $a_tmp[2][$ii];
+            if (empty($a_chps))
+                throw new Model\ChaptersListingNotFoundException(array('volume' => $s_vol));
+            $this->chapters[$s_vol] = $a_chps;
         }
+        while (true);
         if (empty($this->chapters))
             throw new Model\ChaptersListingNotFoundException;
         return $this;
